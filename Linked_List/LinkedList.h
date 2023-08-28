df8cda462cc469219450ebd1603d6b107618df4b
Nam's_Desktop <vbnb0123@naver.com>
2023-08-28 19:22:02 +0900

    Double Linked List 제작 시작

--- Linked_List/LinkedList.h
+++ Linked_List/LinkedList.h
@@ -22,10 +22,10 @@ public:
 
 public:
 	//Add New Node
-	static Node* Create(T data)
+	static Node* Create(T** data)
 	{
 		Node* node = new Node();
-		node->Data = data;
+		node->Data = *data;
 		node->Next = NULL;
 
 		return node;
@@ -40,7 +40,7 @@ public:
 	//Remove Node With Index
 	static void Destroy(int location)
 	{
-		Node* current = head;
+		Node* current = m_Head;
 		Node* prev;
 		Node* next;
 		int count = 0;
@@ -62,13 +62,14 @@ public:
 	//Push Node to End
 	void Push(Node* node)
 	{
-		if (head == NULL)
+		if (m_Head == NULL)
 		{
 			node->Next = NULL;
-			head = node;
+			m_Head = node;
+			return;
 		}
 
-		Node* tail = head;
+		Node* tail = m_Head;
 		while (tail->Next != NULL)
 			tail = tail->Next;
 		tail->Next = node;
@@ -78,12 +79,12 @@ public:
 	//	Pop to End
 	void Pop_Back()
 	{
-		if (head == NULL)
+		if (m_Head == NULL)
 		{
 			return;
 		}
 
-		Node* tail = head;
+		Node* tail = m_Head;
 		Node* prev = NULL;
 		while (tail->Next != NULL)
 		{
@@ -104,7 +105,7 @@ public:
 	//Insert Node to Behind Location Index
 	void Insert(int location, Node* node)
 	{
-		Node* current = head;
+		Node* current = m_Head;
 		int count = 0;
 		while (current->Next != NULL && count++ != location)
 			current = current->Next;
@@ -114,18 +115,18 @@ public:
 	//Delete Node from Node
 	void Delete(Node* node)
 	{
-		if (head == NULL)
+		if (m_Head == NULL)
 		{
 			return;
 		}
 
-		if (head->Data == node->Data)
+		if (m_Head->Data == node->Data)
 		{
-			delete head;
-			head = NULL;
+			delete m_Head;
+			m_Head = NULL;
 		}
 
-		Node* findNode = head;
+		Node* findNode = m_Head;
 		Node* prev = NULL;
 		Node* next = NULL;
 		while (findNode->Next != NULL)
@@ -151,25 +152,26 @@ public:
 	//Delete Node from Location
 	void Delete(int location)
 	{
-		if (GetNodeCount() < location)
+		if (GetSize() < location)
 		{
 			return;
 		}
 
-		if (head == NULL)
+		if (m_Head == NULL)
 		{
 			return;
 		}
 
 		if (location == 0)
 		{
-			Node* next = head->Next;
-			delete head;
-			head = next;
+			Node* next = m_Head->Next;
+			delete m_Head->Data;
+			delete m_Head;
+			m_Head = next;
 			return;
 		}
 
-		Node* current = head;
+		Node* current = m_Head;
 		Node* prev = NULL;
 		Node* next = NULL;
 		int count = 0;
@@ -186,13 +188,14 @@ public:
 		}
 
 		prev->Next = next;
+		delete current->Data;
 		delete current;
 	}
 
 	//Get Node from Location Index
 	Node* GetNode(int location)
 	{
-		Node* current = head;
+		Node* current = m_Head;
 		int count = 0;
 		while (current->Next != NULL && count++ != location)
 			current = current->Next;
@@ -200,9 +203,9 @@ public:
 	}
 
 	//Get Node Count
-	int GetNodeCount()
+	int GetSize()
 	{
-		Node* current = head;
+		Node* current = m_Head;
 		int count = 0;
 		while (current->Next != NULL)
 		{
@@ -217,18 +220,18 @@ public:
 	{
 		if (node == NULL)
 		{
-			if (head != NULL)
+			if (m_Head != NULL)
 			{
-				cout << head->Data;
-				if (head->Next != NULL)
+				cout << *m_Head->Data;
+				if (m_Head->Next != NULL)
 				{
 					cout << " -> ";
-					Print(head->Next);
+					Print(m_Head->Next);
 				}
 			}
 			return;
 		}
-		cout << node->Data;
+		cout << *node->Data;
 		if (node->Next != NULL)
 		{
 			cout << " -> ";
@@ -245,12 +248,216 @@ public:
 	struct Node
 	{
 		//Linked List Data
-		T Data;
+		T* Data;
 		//Linked List Next Node Pointer
 		Node* Next;
 	};
 
 private:
 	//Add Linked List Head Node
-	Node* head = NULL;
+	Node* m_Head = NULL;
+};
+
+template<typename T>
+class DoubleLinkedList
+{
+public:
+	struct Node;
+
+public:
+	//Add New Node
+	static Node* Create(T** data)
+	{
+		Node* node = new Node();
+		node->Data = *data;
+		node->Next = NULL;
+
+		return node;
+	};
+
+public:
+	bool SetCurNode()
+	{
+		return false;
+	}
+
+	void Push_Back(Node* node)
+	{
+		if (m_HeadNode == NULL)
+		{
+			node->Next = NULL;
+			node->Prev = NULL;
+			m_HeadNode = node;
+			m_CurNode = node;
+			return;
+		}
+
+		m_CurNode->Next = node;
+		node->Prev = m_CurNode;
+		node->Next = NULL;
+
+		m_CurNode = node;
+	}
+
+	void Push_Cur(Node* node)
+	{
+		if (m_CurNode == NULL)
+		{
+			return;
+		}
+
+		if (m_CurNode->Next != NULL)
+		{
+			Node* nextNode = m_CurNode->Next;
+			nextNode = node;
+			nextNode->Prev = node;
+			node->Prev = m_CurNode;
+			node->Next = nextNode;
+			m_CurNode = node;
+		}
+		else
+		{
+			m_CurNode->Next = node;
+			node->Prev = m_CurNode;
+			m_CurNode = node;
+		}
+	}
+
+	Node* Pop_Back()
+	{
+		if (m_HeadNode == NULL)
+		{
+			return NULL;
+		}
+
+		while (m_CurNode->Next != NULL)
+		{
+			m_CurNode = m_CurNode->Next;
+		}
+
+		Node* popNode = m_CurNode;
+
+		m_CurNode->Prev->Next = NULL;
+
+		m_CurNode = m_CurNode->Prev;
+
+		return popNode;
+	}
+
+	Node* Pop_Cur()
+	{
+		if (m_CurNode == NULL)
+		{
+			return NULL;
+		}
+
+		Node* popNode = m_CurNode;
+
+		if (m_CurNode->Next != NULL)
+		{
+			m_CurNode->Prev->Next = m_CurNode->Next;
+			m_CurNode->Next->Prev = m_CurNode->Prev;
+			m_CurNode = m_CurNode->Prev;
+		}
+		else
+		{
+			m_CurNode->Prev->Next = NULL;
+			m_CurNode = m_CurNode->Prev;
+		}
+
+		return popNode;
+	}
+
+	void RemoveAll()
+	{
+		if (m_HeadNode == NULL)
+		{
+			return;
+		}
+
+		if (m_HeadNode->Next == NULL)
+		{
+			delete m_HeadNode->Data;
+			delete m_HeadNode;
+
+			m_HeadNode = NULL;
+			m_CurNode = NULL;
+			return;
+		}
+
+		while (m_CurNode != NULL)
+		{
+			Node* temp = m_CurNode;
+			m_CurNode = m_CurNode->Next;
+			delete temp->Data;
+			delete temp;
+		}
+
+
+		m_HeadNode = NULL;
+		m_CurNode = NULL;
+	}
+
+	int GetSize()
+	{
+		if (m_HeadNode == NULL)
+		{
+			return 0;
+		}
+
+		Node* End = m_HeadNode;
+		int Count = 1;
+
+		while (End->Next != NULL)
+		{
+			End = End->Next;
+			Count++;
+		}
+
+		return Count;
+	}
+
+	//Print Node <Àç±Í ÇÔ¼ö>
+	void Print(Node* node = NULL)
+	{
+		if (node == NULL)
+		{
+			if (m_HeadNode != NULL)
+			{
+				cout << *m_HeadNode->Data;
+				if (m_HeadNode->Next != NULL)
+				{
+					cout << " -> ";
+					Print(m_HeadNode->Next);
+				}
+			}
+			return;
+		}
+		cout << *node->Data;
+		if (node->Next != NULL)
+		{
+			cout << " -> ";
+			Print(node->Next);
+		}
+		else
+		{
+			cout << endl;
+		}
+	}
+
+public:
+	//Struct of Linked List Node
+	struct Node
+	{
+		//Linked List Data
+		T* Data;
+		Node* Prev;
+		//Linked List Next Node Pointer
+		Node* Next;
+	};
+
+private:
+	//Add Linked List Head Node
+	Node* m_HeadNode = NULL;
+	Node* m_CurNode = NULL;
 };
