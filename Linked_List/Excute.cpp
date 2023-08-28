02dec9e4a4716998626fb52a7ca717a4bc277503
Nam's_Desktop <vbnb0123@naver.com>
2023-08-28 19:22:20 +0900

    double Linked List 실행 코드 변경

--- Linked_List/Excute.cpp
+++ Linked_List/Excute.cpp
@@ -1,18 +1,25 @@
 #include "LinkedList.h"
 
 typedef LinkedList<int> List;
+typedef DoubleLinkedList<int> DoubleList;
 
 int main()
 {
-	List* list = new List();
-	for(int i = 1; i <= 7; i++)
-		list->Push(List::Create(i));
+	/*List* list = new List();
+	for (int i = 1; i <= 7; i++)
+	{
+		int* index = new int;
+		*index = *(&i);
+		list->Push(List::Create(&index));
+	}
 
 	list->Print();
 
-	cout << list->GetNodeCount() << endl << endl;
+	cout << list->GetSize() << endl << endl;
 
-	list->Insert(3, List::Create(20));
+	int* index = new int;
+	*index = 20;
+	list->Insert(3, List::Create(&index));
 
 	list->Print();
 
@@ -26,7 +33,38 @@ int main()
 	list->Print();
 
 	list->Delete(4);
-	list->Print();
+	list->Print();*/
+
+	DoubleList* doubleList = new DoubleList;
+	for (int i = 1; i <= 7; i++)
+	{
+		int* index = new int;
+		*index = *(&i);
+		doubleList->Push_Back(DoubleList::Create(&index));
+	}
+
+	doubleList->Print();
+
+	cout << doubleList->GetSize() << endl << endl;
+
+	int* index = new int;
+	*index = 20;
+	doubleList->Push_Cur(DoubleList::Create(&index));
+
+	doubleList->Print();
+
+	cout << doubleList->GetSize() << endl << endl;
+
+	DoubleList::Node* node = doubleList->Pop_Back();
+	cout << *node->Data << endl;
+
+	doubleList->Print();
+
+	node = doubleList->Pop_Cur();
+	cout << *node->Data << endl;
+
+	doubleList->Print();
+
 	
 	return 0;
 }
